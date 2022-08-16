ad736ed86d4c7bde61a7e683bace9250217cb8d7
S-Shinich <36124612+S-Shinich@users.noreply.github.com>
2022-08-16 22:23:09 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.greatVal = max;
+        this.minVal = min;
+        this.node = null;
     }
 
     guess() {
-
+       return this.node = Math.round((this.greatVal - this.minVal) / 2 ) + this.minVal; 
     }
 
     lower() {
-
+        return  this.greatVal = this.node;
     }
 
     greater() {
-
+        return this.minVal = this.node;
     }
 }
 
